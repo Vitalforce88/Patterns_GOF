2d505b9e0b31bc177138a42af8346da6b8387205
Vitalforce88 <brian1694@gmail.com>
2022-04-03 13:37:38 +0300

    Сделал Singleton

---
+++ SBomberProject/SBomberProject/SBomberProject.cpp
@@ -0,0 +1,41 @@
+﻿
+#include <conio.h>
+#include <chrono>
+#include <thread>
+#include "SBomber.h"
+#include "MyTools.h"
+
+using namespace std;
+
+//========================================================================================================================
+
+int main(void)
+{
+    MyTools::LogSingleton::getInstance().OpenLogFile("log.txt");
+
+    SBomber game;
+
+    do {
+        game.TimeStart();
+
+        if (_kbhit())
+        {
+            game.ProcessKBHit();
+        }
+
+        std::this_thread::sleep_for(500ms);
+
+        MyTools::ClrScr();
+
+        game.DrawFrame();
+        game.MoveObjects();
+        game.CheckObjects();
+
+        game.TimeFinish();
+
+    } while (!game.GetExitFlag());
+
+    MyTools::LogSingleton::getInstance().CloseLogFile();
+
+    return 0;
+}
